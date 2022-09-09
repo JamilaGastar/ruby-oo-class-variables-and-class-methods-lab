8dc16d523e56793888463508191aabdaed706b88
jamilagastar <jamilagastar@gmail.com>
2022-09-09 22:02:39 +1000

    partially completed lab

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end 
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count(genre, count)
+        genre_count = {}
+        genre_count[
+            self.genres.select{|genre| Song.genre}
+        ]
+    end
+
+    def artist_count
+    end
+
+end
